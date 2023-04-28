9624c27d37d9bfe5c4287982c8de23e734e4b01d
Asif Iqbal <asifsyed71@hotmail.com>
2023-04-29 02:29:14 +0530

    Added persist library to persist cart state

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,23 @@
 import {compose,createStore, applyMiddleware} from "redux";
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import { rootReducer } from "./root-reducer";
 
 //root reducer
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares))
-export const store = createStore(rootReducer,undefined,composedEnhancers);
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    blaclist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
+//Enhancer for Redux Toolkit
+const composeEnhancerRedux = (process.env.NODE_ENV !== 'production' && window && window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers = composeEnhancerRedux(applyMiddleware(...middleWares))
+export const store = createStore(persistedReducer,undefined,composedEnhancers);
+
+export const persistor = persistStore(store);
